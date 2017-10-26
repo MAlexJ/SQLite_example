4b59724b8c0caca360e03fe804317c615d6f60f5
malexj <alexmaximovj@gmail.com>
2017-10-26 22:07:11 +0300

    SQL-APP-001: added logger

--- src/main/java/com/malexj/app/controller/Controller.java
+++ src/main/java/com/malexj/app/controller/Controller.java
@@ -1,4 +1,12 @@
 package com.malexj.app.controller;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Controller {
+
+	public static void main(String[] args) {
+		log.debug("someMethod started");
+	}
+
 }
--- src/main/java/com/malexj/app/model/User.java
+++ src/main/java/com/malexj/app/model/User.java
@@ -1,4 +1,20 @@
 package com.malexj.app.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+
+@Data
+@EqualsAndHashCode(exclude = {"userId"})
 public class User {
+
+	@NonNull
+	private long userId;
+
+	private String fullName;
+
+	private String email;
+
+	private String password;
+
 }
