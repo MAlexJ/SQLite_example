8a7eb9b4f48e36d0ce413334f65ecf07281c8b6d
HERE\maximov <ext-alexey.maximov@here.com>
2018-03-16 15:50:59 +0200

    fix js controller

--- src/main/resources/static/js/components/dcl.js
+++ src/main/resources/static/js/components/dcl.js
@@ -8,18 +8,17 @@ app.component('dcl', {
                    <div class="row">
                        <nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
                            <ul class="nav nav-pills flex-column">
-                                <li class="nav-item" ng-repeat="subCategory in  $ctrl.dcl.data track by $index">
-                                   <a class="nav-link" ui-sref-active="active" ui-sref="dclSubCategory({ id: subCategory.id })">
-                                           {{subCategory.name}}
+                                <li class="nav-item" ng-repeat="subCategory in  $ctrl.dcl.data.subCategories track by $index">
+                                   <a class="nav-link" ui-sref-active="active" ui-sref="dclSubCategory({ id: subCategory.idSubCategory })">
+                                           {{subCategory.nameSubCategory}}
                                     </a>
                                 </li>
                            </ul>
                        </nav>
                        <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
-                           <h1>Text</h1>
-                           <section class="row text-center placeholders">
-                               <ui-view>{{$ctrl.dcl.data}}</ui-view>
-                           </section>
+                            <ui-view>
+                                 <div ng-bind-html="$ctrl.dcl.data.html"></div>                               
+                            </ui-view>
                        </main>
                    </div>
                </div>
--- src/main/resources/static/js/components/tcl.js
+++ src/main/resources/static/js/components/tcl.js
@@ -8,18 +8,17 @@ app.component('tcl', {
                 <div class="row">
                     <nav class="col-sm-3 col-md-2 hidden-xs-down bg-faded sidebar">
                         <ul class="nav nav-pills flex-column">
-                             <li class="nav-item" ng-repeat="tclSubCategory in  $ctrl.tcl.data track by $index">
-                                <a class="nav-link" ui-sref-active="active" ui-sref="tclSubCategory({ id: tclSubCategory.id })">
-                                        {{tclSubCategory.name}}
+                             <li class="nav-item" ng-repeat="subCategory in  $ctrl.tcl.data.subCategories track by $index">
+                                <a class="nav-link" ui-sref-active="active" ui-sref="tclSubCategory({ id: subCategory.idSubCategory })">
+                                        {{subCategory.nameSubCategory}}
                                  </a>
                              </li>
                         </ul>
                     </nav>
                     <main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3">
-                        <h1>Text</h1>
-                        <section class="row text-center placeholders">
-                            <ui-view>{{$ctrl.tcl.data}}</ui-view>
-                        </section>
+                        <ui-view>
+                            <div ng-bind-html="$ctrl.tcl.data.html"></div>                               
+                        </ui-view>
                     </main>
                 </div>
             </div>
